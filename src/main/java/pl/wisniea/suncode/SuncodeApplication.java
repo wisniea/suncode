1e8f1706d863c6f63fc67572863efc28944107c3
wisniea <masterflip9999@gmail.com>
2022-03-29 22:01:27 +0200

    Init

---
+++ src/main/java/pl/wisniea/suncode/SuncodeApplication.java
@@ -0,0 +1,13 @@
+package pl.wisniea.suncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuncodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuncodeApplication.class, args);
+    }
+
+}
